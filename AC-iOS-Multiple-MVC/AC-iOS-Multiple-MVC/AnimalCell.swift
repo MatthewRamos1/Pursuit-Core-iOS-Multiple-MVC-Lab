37b47cdeee43acfedc4c2f2c61cf2bdf6dc14311
Matthew Ramos <mmramos309@gmail.com>
2019-11-18 16:03:35 -0500

    Embeded vc in navigation controller, added prototype cell and started on custom cell file

---
+++ AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/AnimalCell.swift
@@ -0,0 +1,15 @@
+//
+//  AnimalCell.swift
+//  AC-iOS-Multiple-MVC
+//
+//  Created by Matthew Ramos on 11/18/19.
+//  Copyright © 2019 AC-iOS. All rights reserved.
+//
+
+import UIKit
+
+class AnimalCell: UITableViewCell {
+
+
+
+}
